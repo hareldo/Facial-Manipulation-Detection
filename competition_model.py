443cd9bffb316402ce94c6dbb9a4479703e5a4c2
elliotTAU <62844891+elliotTAU@users.noreply.github.com>
2023-01-14 20:35:36 +0200

    competition part

--- competition_model.py
+++ competition_model.py
@@ -1,15 +1,27 @@
 """Define your architecture here."""
 import torch
 from models import SimpleNet
+from torch import nn
+import torchvision.models as models
 
 
-def my_competition_model():
+def my_competition_model(model_args,load):
     """Override the model initialization here.
 
     Do not change the model load line.
     """
-    # initialize your model:
-    model = SimpleNet()
+    
+
+
+    model = models.regnet_x_400mf(pretrained=True)
+    model.classifier = nn.Sequential(
+        nn.Linear(576, 256),
+        nn.ReLU(),
+        nn.Linear(256, 64),
+        nn.ReLU(),
+        nn.Linear(64, 2))
+
+
     # load your model using exactly this line (don't change it):
-    model.load_state_dict(torch.load('checkpoints/competition_model.pt')['model'])
+    model.load_state_dict(torch.load('checkpoints/fakes_dataset_my_competition_model_Adam.pt')['model'])
     return model
