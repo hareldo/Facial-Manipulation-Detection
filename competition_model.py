e7486cd1305a1d274db4075c3ac74f3010ee6547
elliotTAU <62844891+elliotTAU@users.noreply.github.com>
2023-01-25 12:16:09 +0200

    Add files via upload
    fix network efficeient net score 11.3

--- competition_model.py
+++ competition_model.py
@@ -4,24 +4,21 @@ from models import SimpleNet
 from torch import nn
 import torchvision.models as models
 
-
 def my_competition_model():
     """Override the model initialization here.
 
     Do not change the model load line.
     """
-    
-
-
-    model = models.shufflenet_v2_x1_0(pretrained=True)
+    # initialize your model:
+    model = models.efficientnet_b0(pretrained=True)
     model.classifier = nn.Sequential(
+        nn.Linear(1280, 576),
+        nn.ReLU(),
         nn.Linear(576, 256),
         nn.ReLU(),
         nn.Linear(256, 64),
         nn.ReLU(),
         nn.Linear(64, 2))
-
-
     # load your model using exactly this line (don't change it):
-    model.load_state_dict(torch.load('checkpoints/fakes_dataset_my_competition_model_Adam.pt')['model'])
+    model.load_state_dict(torch.load('checkpoints/competition_model.pt')['model'])
     return model
